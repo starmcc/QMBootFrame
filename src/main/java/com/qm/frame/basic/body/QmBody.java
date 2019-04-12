8f466cdf52970a112e85e3d1ebc62c86fd946913
starmcc <starczt1992@163.com>
2019-04-12 15:01:50 +0800

    v0.7.5

--- src/main/java/com/qm/frame/basic/body/QmBody.java
+++ src/main/java/com/qm/frame/basic/body/QmBody.java
@@ -20,11 +20,6 @@ public @interface QmBody {
     boolean required() default true;
  
     /**
-     * 当value的值或者参数名不匹配时，是否允许解析最外层属性到该对象
-     */
-    boolean parseAllFields() default true;
- 
-    /**
      * 解析时用到的JSON的key
      */
     String key() default "";
